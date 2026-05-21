4224f18e426a6b5c10e376a6439af56ff27121ca
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-05-21 11:17:34 +0600

    mongodb connnected

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+const dns= require('node:dns') ;
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+require('dotenv').config()
+
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MongoDB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('')
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
