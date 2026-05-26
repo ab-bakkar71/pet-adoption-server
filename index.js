f58f1af09b68a7435f3f569c3017c0b29a128b46
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-05-26 07:27:08 +0600

    post method for adopt pet

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+app.use(cors());
+app.use(express.json())
 
+
+// Connection URI
 const uri = process.env.MongoDB_URI;
+
+// mongoDB client connection
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,22 +27,53 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db('')
+    const database = client.db('pet-adoption');
+    const pets = database.collection('pets');
+
+    // send data on dataBase
+
+    app.post('/adopt', async(req, res) =>{
+      const adopt = req.body;
+      console.log('fontend data', adopt);
+      const result = await pets.insertOne(adopt);
+      res.send(result)
+
+
+    })
+
+
+  // get all data
+    app.get('/pets', async (req, res) => {
+        const cursor = pets.find();
+        const results = await cursor.toArray();
+        res.json(results);
+    });
+
+  // for feature
+
+  app.get('/feature', async(req, res)=>{
+    const cursor = pets.find().limit(8);
+    const result = await cursor.toArray();
+    res.send(result)
+  })
+
+    // get one data by id
+    app.get ('/pets/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const pet = await pets.findOne(query);
+      res.send(pet);
+    })
+
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
